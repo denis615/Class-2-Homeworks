e076d6392ecd3e811ec9248399db045fa4b5be5b
denis615 <denis615@gmail.com>
2020-04-14 18:22:13 +0200

    testing commit

---
+++ Covid front end/covid19.js
@@ -0,0 +1,23 @@
+let about=document.getElementById("about")
+
+about.addEventListener(`click`,function(){
+    let header1=document.getElementById("header1")
+    header1.innerText="Welcome to the Covid-19 Info Website"
+    let header6=document.getElementById("header6")
+    header6.innerText="This website was built by: denis615@gmail.com"
+    let paragraph=document.getElementById("paragraphabout")
+    paragraph.innerText="These are some of the things you can do to protect yourself from Covid-19:"
+    let advice1=document.getElementById("advice1")
+    advice1.innerText="1.Wash your hands frequently Regularly and thoroughly clean your hands with an alcohol-based hand rub or wash them with soap and water"
+    let advice2=document.getElementById("advice2")
+    advice2.innerText="2.Maintain at least 1 metre (3 feet) distance between yourself and anyone who is coughing or sneezing."
+    let advice3=document.getElementById("advice3")
+    advice3.innerText="3.Avoid touching eyes, nose and mouth"
+    let advice4=document.getElementById("advice4")
+    advice4.innerText="4.Make sure you, and the people around you, follow good respiratory hygiene. This means covering your mouth and nose with your bent elbow or tissue when you cough or sneeze. Then dispose of the used tissue immediately."
+    let advice5=document.getElementById("advice5")
+    advice5.innerText="5.Stay home if you feel unwell. If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow the directions of your local health authority."
+    let advice6=document.getElementById("advice6")
+    advice6.innerText="6.Stay informed on the latest developments about COVID-19. Follow advice given by your healthcare provider, your national and local public health authority or your employer on how to protect yourself and others from COVID-19."
+
+})
